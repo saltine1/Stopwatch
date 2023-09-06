fb3ee8bcad0f56f18116e9c60cb6aa5107e1a62b
saltine1 <sophialiu.mail@gmail.com>
2023-09-06 09:20:24 -0700

    at least it's not pi = 5

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -10,12 +10,13 @@ class MainActivity : AppCompatActivity() {
     companion object{
         // TAG is the default var name for labeling log statements
         val TAG = "MainActivity"
+        // github test
+        val ASTROPHYSICISTS_PI = 3
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate: this is a log")
-        println("Hi!")
     }
     // to override an existing function, just start typing it
     // can do this for the rest of the lifecycle functions (or any function
