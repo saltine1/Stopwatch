048f09ad9f1e43666f070c3336654ebae97719d2
saltine1 <sophialiu.mail@gmail.com>
2023-09-08 08:44:41 -0700

    added logs

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager.OnActivityStopListener
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -11,16 +12,17 @@ class MainActivity : AppCompatActivity() {
         // TAG is the default var name for labeling log statements
         val TAG = "MainActivity"
         val VERBOSE = "oui oui"
-        val DEBUG = ""
+        val DEBUG = "il y a un probleme"
         val INFO = ""
         val WARN = "ton code est en train de mourir"
         val ERROR = "ton code est mort"
         val WTF = "ton code s'est décomposé"
+        val LIFECYCLE = "lifecycle methods"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate: this is a log")
+        Log.d(LIFECYCLE, "create")
     }
     // to override an existing function, just start typing it
     // can do this for the rest of the lifecycle functions (or any function
@@ -28,17 +30,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart(){
         super.onStart()
+        Log.i(LIFECYCLE, "start")
     }
     override fun onResume(){
         super.onResume()
+        Log.i(LIFECYCLE, "resume")
     }
     override fun onPause(){
         super.onPause()
+        Log.i(LIFECYCLE, "pause")
     }
     override fun onStop(){
         super.onStop()
+        Log.i(LIFECYCLE, "stop")
     }
     override fun onDestroy(){
         super.onDestroy()
+        Log.i(LIFECYCLE, "destroy")
     }
 }
